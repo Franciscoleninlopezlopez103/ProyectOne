02f9c12ac55e41735eab7147500725fb6a3f3196
Franciscoleninlopezlopez103 <127366368+Franciscoleninlopezlopez103@users.noreply.github.com>
2023-06-14 22:45:39 -0600

    Add files via upload

---
+++ Ejercicio1.php
@@ -0,0 +1,21 @@
+<?php 
+$Uusario="Esquivel";
+$Password="54321";
+$User=$_POST['Usuario'];
+$Pass=$_POST['Password'];
+if ($User=="    "and $Pass=="     "){
+    echo "los datos estan llenos";
+}   
+else?
+if($Usuario==$User and $Password==$Pass){
+    echo "Bienvenido" $User;
+
+}
+else? 
+echo "Datos incompletos";
+$Contacto;
+if ($Contacto>3){
+    echo "adios";
+}
+
+
